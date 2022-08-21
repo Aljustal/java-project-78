252c1fc8fb4b937cf1d16f56355ef351f1bd44a6
Aljustal <orbit.fsk@yandex.ru>
2022-08-22 00:23:24 +0300

    add 1st test

--- src/test/java/app/AppTest.java
+++ src/test/java/app/AppTest.java
@@ -1,4 +1,16 @@
-package java.app;
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
+    @Test
+    void testTest()  {
+
+        String actual = ("hi");
+        String expectedString = ("hi");
+
+        assertThat(actual).isEqualTo(expectedString);
+    }
 }
